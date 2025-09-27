09ef52137bc2defba87b78e3d314ae8af0c711bd
Kloud6 Dev <mubarak-cod>
2025-09-27 18:43:27 +0100

    adding lost files

---
+++ src/components/LoadingAnimation.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const LoadingAnimation = () => {
+  return (
+    <div>
+         <DotLottieReact
+      src="https://lottie.host/0f678c21-7fd7-4d74-9174-8b9583b19815/SXYnCtGkeV.lottie"
+      loop
+      autoplay
+    />
+    </div>
+  )
+}
+
+export default LoadingAnimation;
