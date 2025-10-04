81eead622612e841a8cdbdae31b33968c20241b4
Kloud6 Dev <mubarak-cod>
2025-10-04 13:43:23 +0100

    updated code

--- src/app/aboutUs/page.tsx
+++ src/app/aboutUs/page.tsx
@@ -8,7 +8,7 @@ import Header from "@/components/Header";
 const page = () => {
   return (
     <div className={styles.aboutcont}>
-      {/* <Header /> */}
+      <Header />
       <AboutKICA />
       <Mission />
       <CTACard />
