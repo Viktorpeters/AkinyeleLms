bb318f24c24a7bc7b1a969a45028d290aabd2783
david nwokedike <miklileza@gmail.com>
2025-10-11 01:30:25 +0100

    Created a new branch => v1

--- frontend/src/app/(auth)/signin/page.tsx
+++ frontend/src/app/(auth)/signin/page.tsx
@@ -25,18 +25,23 @@ export default function SignIn() {
       <div className="illustrationContainer">
         <div className="overlay"></div>
         <div className="illustrationn">
-
-        <img
-          src="https://res.cloudinary.com/dcghgoebb/image/upload/v1758146718/Data_security_02_g7l54e.jpg"
-          alt="Sign In Illustration"
-          className="illustratio"
-        />
+          <img
+            src="https://res.cloudinary.com/dcghgoebb/image/upload/v1758146718/Data_security_02_g7l54e.jpg"
+            alt="Sign In Illustration"
+            className="illustratio"
+          />
         </div>
       </div>
 
       <div className="card">
         <div className="logoWrapper">
-          <img src="https://res.cloudinary.com/ddlnqthao/image/upload/v1758300104/WhatsApp_Image_2025-09-19_at_17.19.02_cb5c3139_jazl8s.jpg" width={60} height={40} alt="Logo" className="log" />
+          <img
+            src="https://res.cloudinary.com/ddlnqthao/image/upload/v1758300104/WhatsApp_Image_2025-09-19_at_17.19.02_cb5c3139_jazl8s.jpg"
+            width={60}
+            height={40}
+            alt="Logo"
+            className="log"
+          />
         </div>
 
         <div className="header">
